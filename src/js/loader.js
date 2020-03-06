cc3cd3489e7b5a272ec1b4edda28892ad00c2002
antoniolopesjr <antonio.lopesjr@outlook.com>
2020-03-06 13:05:06 -0300

    alterações estrutura

---
+++ src/js/loader.js
@@ -0,0 +1,17 @@
+/*$(window).load(function() {
+    $("#loader").delay(0).animate({
+        'opacity': 0
+    }, 800, null, function() {
+        $('#loader').fadeOut('slow');
+    });
+});
+
+
+$(document).ready(function() {
+
+    setTimeout(function() {
+        $('body').addClass('loaded');
+        /*$('h1').css('color', '#222222');
+    }, 500);
+
+});*/
---
+++ src/js/main.js
@@ -0,0 +1,112 @@
+/*FUNCAO QUE CONTROLE O EFEITO DO MENU SUPERIOR*/
+jQuery(window).on("load", function() {
+
+    jQuery(window).scroll(function() {
+        var scroll = jQuery(window).scrollTop();
+
+        if (scroll >= 500) {
+            jQuery(".navbar-box").addClass("ativar");
+            jQuery(".menu-top").addClass("fade");
+            jQuery(".navbar").addClass("solid");
+
+        } else if (scroll <= 20) {
+            jQuery(".navbar-box").removeClass("ativar");
+            jQuery(".menu-top").removeClass("fade");
+            jQuery(".navbar").removeClass("solid");
+
+        }
+
+    });
+});
+
+jQuery(window).on("load", function() {
+
+    jQuery(window).scroll(function() {
+        var scroll = jQuery(window).scrollTop();
+        if (scroll >= 700) {
+            jQuery(".logo-navbar").addClass("aparece");
+        } else if (scroll <= 30) {
+            jQuery(".logo-navbar").removeClass("aparece");
+        }
+
+    });
+});
+
+
+
+window.onbeforeunload = function() {
+        window.scrollTo(0, 0);
+    }
+    //indicador de scroll da pagina
+    /*jQuery(window).on("load", function() {
+        window.onscroll = function() {
+            myFunction()
+        };
+
+        function myFunction() {
+            var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+            var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+            var scrolled = (winScroll / height) * 100;
+            document.getElementById("myBar").style.width = scrolled + "%";
+        }
+    });*/
+
+
+
+//var transitionEnd = 'webkitTransitionEnd otransitionendo TransitionEnd msTransitionEnd transitionend';
+
+jQuery(window).on("load", function() {
+
+    jQuery(window).scroll(function() {
+        var scroll = jQuery(window).scrollTop();
+
+        if (scroll >= 900) {
+            //jQuery(".whats-icon").addClass("aparece");
+            $('.whats-icon')
+                .addClass('aparece')
+                //.one(transitionEnd, doSomethingAfterIt);
+        } else if (scroll <= 20) {
+            //jQuery(".whats-icon").removeClass("aparece");
+            $('.whats-icon')
+                .removeClass('aparece')
+                //.one(transitionEnd, doSomethingAfterIt);
+        }
+    });
+});
+
+
+
+
+jQuery('.scroller').click(function() {
+
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = jQuery(this.hash);
+        target = target.length ? target : jQuery('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            jQuery('html, body').animate({
+                scrollTop: target.offset().top - 90
+            }, 1000);
+            return false;
+        }
+    }
+
+});
+
+
+$(window).on('load', function() { // makes sure the whole site is loaded 
+    $('#camera').fadeOut(); // will first fade out the loading animation 
+    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+    $('body').delay(350).css({
+        'overflow': 'visible'
+    });
+})
+
+
+$(document).ready(function() {
+
+    // Fakes the loading setting a timeout
+    setTimeout(function() {
+        $('body').addClass('loaded');
+    }, 1400);
+
+});
